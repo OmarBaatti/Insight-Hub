ce5234be0b26235d77e5790d36134bce33f3e873
Omar-Baatti <baattiomar@gmail.com>
2026-07-05 13:35:33 +0200

    Components initialization

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import Header  from "../components/Header"
+
 export default function Home() {
   return (
-    <div className="p-8">
-      <button className="btn btn-primary">
-        DaisyUI works!
-      </button>
-    </div>
+    <>
+      <Header/>
+      <div className="p-8">
+        <button className="btn btn-primary">
+          DaisyUI works!
+        </button>
+      </div>
+    </>
   );
 }
